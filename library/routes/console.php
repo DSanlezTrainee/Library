0a6bc2a1dd4cc4cf14e0ddeccdec2cd47868f40c
Diogo <diogo.sanlez@trainee.inovcorp.com>
2025-08-18 12:36:00 +0100

    fix

--- library/routes/console.php
+++ library/routes/console.php
@@ -2,11 +2,10 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Artisan::command('schedule', function ($schedule) {
-    $schedule->command('app:send-book-return-reminders')->dailyAt('00:00');
-});
+Schedule::command('app:send-book-return-reminders')->dailyAt('09:00');
